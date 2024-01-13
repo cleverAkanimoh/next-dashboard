138794de4df29c35958a7bccf4fbe1c5b1dcf080
= <=>
2024-01-13 21:20:36 +0100

    modified data.ts in lib

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -41,7 +41,7 @@ export async function fetchLatestInvoices() {
       ORDER BY invoices.date DESC
       LIMIT 5`;
 
-    const latestInvoices = data.rows.map((invoice) => ({
+    const latestInvoices = data.rows.map((invoice: { amount: number; }) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
