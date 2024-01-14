16a53a3a5fdb5aa9e2602ced18c85839f4ff025b
= <=>
2024-01-14 10:57:34 +0100

    modified charts.js in ui

--- app/ui/charts.js
+++ app/ui/charts.js
@@ -1,16 +1,16 @@
-// import Chart from 'chart.js/auto';
-// import { getRelativePosition } from 'chart.js/helpers';
+import Chart from 'chart.js/auto';
+import { getRelativePosition } from 'chart.js/helpers';
 
-// const chart = new Chart(ctx, {
-//   type: 'line',
-//   data: data,
-//   options: {
-//     onClick: (e) => {
-//       const canvasPosition = getRelativePosition(e, chart);
+const chart = new Chart(ctx, {
+  type: 'line',
+  data: data,
+  options: {
+    onClick: (e) => {
+      const canvasPosition = getRelativePosition(e, chart);
 
-//       // Substitute the appropriate scale IDs
-//       const dataX = chart.scales.x.getValueForPixel(canvasPosition.x);
-//       const dataY = chart.scales.y.getValueForPixel(canvasPosition.y);
-//     }
-//   }
-// });
+      // Substitute the appropriate scale IDs
+      const dataX = chart.scales.x.getValueForPixel(canvasPosition.x);
+      const dataY = chart.scales.y.getValueForPixel(canvasPosition.y);
+    }
+  }
+});
